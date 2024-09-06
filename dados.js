80e42596f0054dba3aeac0290b6cc39267556d6a
Caio Henrique <100355570+CH-dev48@users.noreply.github.com>
2024-09-06 16:53:20 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,18 @@
+let dados = [
+    {titulo: "Volkswagen Fusca",
+    descricao :"Produzido para repovoar as estradas alemãs após a Primeira Guerra Mundial, o Fusca tinha o intuito de ser barato, de fácil manutenção e baixo consumo de combustível. No Brasil, o sucesso do Fusca foi tão grande que o carro chegou a representar 70% da frota nacional na década de 1970.",
+    link : "https://pt.wikipedia.org/wiki/Volkswagen_Fusca" 
+    },
+    {
+        titulo: "Volkswagen Kombi",
+        descricao :"Lançada com o nome de Kombinationsfahrzeug, a Kombi tinha uma missão bastante parecida com a do seu irmão menor, o Fusca, oferecendo uma opção simples e barata para o transporte de cargas e para o lazer.",
+        link : "https://pt.wikipedia.org/wiki/Volkswagen_Kombi" 
+        },
+        {
+            titulo: "CHevrolet Opala",
+            descricao :"Com nome oriundo da fusão entre dois produtos da GM, o Opel e o Impala, o Opala teve diversas versões em terras brasileiras, totalizando quase um milhão de unidades vendidas ao longo de sua trajetória.",
+            link : "https://pt.wikipedia.org/wiki/Chevrolet_Opala" 
+            }
+];
+
+   
